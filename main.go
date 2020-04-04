1bd3abe6529552718a5d0055e2542a18734d175c
Ben Schinn <ben.schinn@mx.com>
2020-04-04 16:18:50 -0600

    built request header

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
     "fmt"
     "net/http"
     "html/template"
+    "net/http"
 )
 
 type Page struct {
@@ -55,6 +56,22 @@ func giftcardPage(w http.ResponseWriter, r *http.Request) {
     fmt.Println("error parsing html template")
   }
   t.Execute(w, data)
+}
+
+func redeemGiftCard(code string) {
+  client := &http.Client{}
+  requestURL := url.URL{
+    Scheme: "https",
+    Host:   "api.giftup.app",
+    Path:   "/gift-cards" + code + "/redeem",
+  }
+
+  requestHeaders := http.Header{
+    "Accept":          {"*/*"},
+    "Content-Type":    {"application/json"},
+    "Accept-Language": {"en-US,en;q=0.9"},
+    "Authorization":   {os.Getenv("API-KEY")}
+  }
 }
 
 func main() {
