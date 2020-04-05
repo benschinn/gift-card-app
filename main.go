3c7d952f91b73cc6f6568103c8e4363e2a1dcd44
Ben Schinn <ben.schinn@mx.com>
2020-04-04 19:54:17 -0600

    supporting port and site-id env var

--- main.go
+++ main.go
@@ -11,8 +11,9 @@ import (
     "io/ioutil"
 )
 
-type Page struct {
+type PageData struct {
   Title string
+  SiteId string
 }
 
 const startTmpl = `
@@ -27,8 +28,9 @@ const startTmpl = `
 
 const endTmpl= `</body></html>`
 
-var data = Page{
+var data = PageData{
   Title: os.Getenv("NAME"),
+  SiteId: os.Getenv("SITEID"),
 }
 
 func landingPage(w http.ResponseWriter, r *http.Request) {
@@ -51,7 +53,7 @@ func redeemPage(w http.ResponseWriter, r *http.Request) {
 
 func giftcardPage(w http.ResponseWriter, r *http.Request) {
   const giftUpTmpl = `
-  <div data-site-id="64756a71-b216-4da0-b16a-c8becfcf7a22" data-platform="Other" class="gift-up-target"></div>
+  <div data-site-id="{{.SiteId}}" data-platform="Other" class="gift-up-target"></div>
   <script type="text/javascript"> (function (g, i, f, t, u, p, s) { g[u] = g[u] || function() { (g[u].q = g[u].q || []).push(arguments) }; p = i.createElement(f); p.async = 1; p.src = t; s = i.getElementsByTagName(f)[0]; s.parentNode.insertBefore(p, s); })(window, document, "script", "https://cdn.giftup.app/dist/gift-up.js", "giftup"); </script>
   `
   t,err := template.New("checkout").Parse(startTmpl + giftUpTmpl + endTmpl)
@@ -99,8 +101,12 @@ func redeemGiftCard(code string, payload string) *http.Response{
 }
 
 func main() {
-    http.HandleFunc("/", landingPage)
-    http.HandleFunc("/redeem", redeemPage)
-    http.HandleFunc("/giftcards", giftcardPage)
-    http.ListenAndServe(":8090", nil)
+  port := "8090"
+  if os.Getenv("PORT") != "" {
+    port = os.Getenv("PORT")
+  }
+  http.HandleFunc("/", landingPage)
+  http.HandleFunc("/redeem", redeemPage)
+  http.HandleFunc("/giftcards", giftcardPage)
+  http.ListenAndServe(":" + port, nil)
 }
